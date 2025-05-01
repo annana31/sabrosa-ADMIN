379ff9a241d4ea8e2d18bb80528cd65ea51d524f
paulaleng <silvestrepaulaouapin@gmail.com>
2025-05-01 23:38:11 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import EmployeeDetails from './pages/EmployeeDetails';
+import AdminProfile from './pages/AdminProfile';
+import AdminPerformance from './pages/AdminPerformance';
+import AdminDashboard from './pages/AdminDashboard';
+import AdminProjects from './pages/AdminProjects';
+import AdminUsers from './pages/AdminUsers';
+import AdminTasks from './pages/AdminTasks';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<EmployeeDetails />} />
+        <Route path="/" element={<AdminProfile />} />
+        <Route path="/dashboard" element={<AdminDashboardDashboard />} />
+        <Route path="/performance" element={<AdminPerformance />} />
+        <Route path="/projects" element={<AdminProjectsProjects />} />
+        <Route path="/users" element={<AdminUsersUsers />} />
+        <Route path="/tasks" element={<AdminTasksTasks />} />
       </Routes>
     </Router>
   );
